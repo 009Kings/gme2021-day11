df360d07d3c47ab743a73c93ea9576bb8ac824d5
009kings <009kings@gmail.com>
2021-02-22 00:30:34 -0800

    pretty auras

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+// Setup
+let buddha = new Image();
+buddha.src = 'https://thebuddhapath.org/wp-content/uploads/2014/08/dzogchen_mandala_92-75x75.png';
 let ctx = display.getContext('2d');
 
-display.width = 800;
-display.height = 400;
+display.width = 4000;
+display.height = 2000;
 
+// Helper functions
 const randomInt = maxNum => {
   return Math.floor(Math.random() * maxNum);
 }
@@ -11,44 +15,53 @@ const moveOne = (toChange, axis, increment) => {
   switch (axis) {
     case 'x':
       // if (toChange > display.width - 5 || toChange < 5) return toChange
-      if (toChange > display.width - 5 && increment) {
+      if (toChange > display.width - 35 && increment) {
         return toChange
-      } else if (toChange < 5 && !increment) {
+      } else if (toChange < 35 && !increment) {
         return toChange
       }
       break;
     default:    
-      if (toChange > display.height - 5 && increment) {
+      if (toChange > display.height - 35 && increment) {
         return toChange
-      } else if (toChange < 5 && !increment) {
+      } else if (toChange < 35 && !increment) {
         return toChange
       }
       break;
   }
-  let mutator = increment ? 1 : -1;
+  let mutator = increment ? 3 : -3;
   return toChange + mutator;
 }
 
+// A dot!
 class Person {
   x = randomInt(display.width);
   y = randomInt(display.height);
   moveRight = Math.random() > .5 ? true : false;
   moveDown = Math.random() > .5 ? true : false;
-  radius = 5;
+  radius = 37.5;
   buddha = false;
-  color = '#404244d6';
   move = function() {
     // Possibly change direction
-    this.moveRight = Math.random() > .9 ? !this.moveRight : this.moveRight;
-    this.moveDown = Math.random() > .9 ? !this.moveDown : this.moveDown;
+    this.moveRight = Math.random() > .95 ? !this.moveRight : this.moveRight;
+    this.moveDown = Math.random() > .95 ? !this.moveDown : this.moveDown;
     this.x = moveOne(this.x, 'x', this.moveRight);
     this.y = moveOne(this.y, 'y', this.moveDown);
   };
   render = function() {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fill();
+    if (this.buddha) {
+      ctx.fillStyle = '#021fff79';
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius + 30, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.drawImage(buddha, this.x - this.radius, this.y - this.radius);
+    } else {
+      ctx.fillStyle = '#afafafde';
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+
   };
   checkCollision = function(x, y) {
     let a = this.x - x;
@@ -59,7 +72,7 @@ class Person {
   }
 }
 
-let population = 500;
+let population = 400;
 
 let people = [];
 
@@ -84,6 +97,6 @@ let movement = setInterval(() => {
       }
     }
   });
-}, 50)
+}, 40)
 
 stopBtn.addEventListener('click', () => clearInterval(movement));
